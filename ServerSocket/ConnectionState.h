722bf72fad080c4a45dee2afa93aa2ac13fe1a4f
bitpaw <nozsbitpaw@gmail.com>
2021-04-26 20:42:04 +0200

    Compiling stuff

---
+++ ServerSocket/ConnectionState.h
@@ -0,0 +1,10 @@
+typedef enum ConnectionState_
+{
+    Invalid,
+
+    Disconnected,
+    Connected,
+
+    Error
+
+}ConnectionState;
--- ServerSocket/main.c
+++ ServerSocket/main.c
@@ -1,5 +1,13 @@
 #include "CommandToken.h"
 
+#define DelaultPort 5678U 
+
+#if 1
+#define ClientMode
+#else
+#define ServerMode
+#endif
+
 int main()
 {
     printf("Hello World");
