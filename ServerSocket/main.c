89fdcc559f307696716d1c19904d16322b1ec305
Dr_ProNOOB <danielmentjukov@web.de>
2021-05-18 17:39:58 +0200

    Fixed spaces problem

--- ServerSocket/main.c
+++ ServerSocket/main.c
@@ -139,7 +139,7 @@ int main()
             {
                 ClearBuffer();
                 //fgets(&inputDataBuffer[0], 40, stdin);
-                scanf("%s", &inputDataBuffer[0]);
+                scanf("%10[0-9a-zA-Z ]", &inputDataBuffer[0]);
 
                 if (memcmp("q", &inputDataBuffer[0], 1) == 0)
                 {
